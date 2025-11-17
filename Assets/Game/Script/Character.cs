11a7196271ad6b19628320a96d2b9dc82e23cec3
BladeBTW <Robbertjanvanommeren@gmail.com>
2025-11-18 00:19:15 +0100

    Basic Movement 02

--- Assets/Game/Script/Character.cs
+++ Assets/Game/Script/Character.cs
@@ -9,6 +9,9 @@ public class Character : MonoBehaviour
     public float MoveSpeed = 5f;
     private Vector3 _movementVelocity;
     private PlayerInput _playerInput;
+    private float _verticalVelocity;
+    public float Gravity = -9.8f;
+
     private void Awake()
     {
         //called when instant of this script is loaded
@@ -22,11 +25,22 @@ public class Character : MonoBehaviour
         _movementVelocity.Normalize();
         _movementVelocity = Quaternion.Euler(0,-45f,0) * _movementVelocity;
         _movementVelocity *= MoveSpeed * Time.deltaTime;
+
+        if (_movementVelocity != Vector3.zero)
+            transform.rotation = Quaternion.LookRotation(_movementVelocity);
     }
 
     private void FixedUpdate()
     {
         CalculatePlayerMovement();
+        //Move characters down when not on the ground
+        //IsGrounded variable in Character Controller, checks if character touched ground last frame
+        if(_cc.isGrounded == false)
+            _verticalVelocity = Gravity;
+        else
+            _verticalVelocity = Gravity * 0.3f;
+        _movementVelocity += _verticalVelocity * Vector3.up * Time.deltaTime;
+
         _cc.Move(_movementVelocity);
     }
 }
