c07fb7e7ceb81f2309b85c9e0ecd50c96bc57861
BladeBTW <Robbertjanvanommeren@gmail.com>
2025-11-18 10:29:10 +0100

    Run animation

--- Assets/Game/Script/Character.cs
+++ Assets/Game/Script/Character.cs
@@ -11,12 +11,14 @@ public class Character : MonoBehaviour
     private PlayerInput _playerInput;
     private float _verticalVelocity;
     public float Gravity = -9.8f;
+    private Animator _animator;
 
     private void Awake()
     {
         //called when instant of this script is loaded
         _cc = GetComponent<CharacterController>();
         _playerInput = GetComponent<PlayerInput>();
+        _animator = GetComponent<Animator>();
     }
 
     private void CalculatePlayerMovement()
@@ -24,6 +26,10 @@ public class Character : MonoBehaviour
         _movementVelocity.Set(_playerInput.HorizontalInput,0f,_playerInput.VerticalInput);
         _movementVelocity.Normalize();
         _movementVelocity = Quaternion.Euler(0,-45f,0) * _movementVelocity;
+        
+
+        _animator.SetFloat("Speed",_movementVelocity.magnitude);
+        
         _movementVelocity *= MoveSpeed * Time.deltaTime;
 
         if (_movementVelocity != Vector3.zero)
